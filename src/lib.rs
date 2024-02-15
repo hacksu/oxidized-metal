9b8e42be8c29fbec22028d919996e9881bb25cf8
Noah Struck <noahstruck71303@gmail.com>
2024-02-15 02:20:18 -0500

    Added some boilerplate code to pass image data from the Web to WASM

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 use wasm_bindgen::prelude::*;
 
+// exposing the console.log function
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+#[wasm_bindgen(js_name = processImage)]
+pub fn process_image(data: Vec<u8>) {
+   let result = image::load_from_memory_with_format(&data, image::ImageFormat::Png);
+   match result {
+      Ok(image) => {
+         log(image.height().to_string().as_str());
+         log(image.width().to_string().as_str());
+      },
+      Err(err) => {
+         log(err.to_string().as_str());
+      }
+   }
 }
